03ee69ed44f1773415f84ef55117e113e743d64c
Hebert <monteiro.hebert@gmail.com>
2026-05-01 15:25:58 -0300

    Adicionamos fixtures para o login

--- test/login.test.js
+++ test/login.test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http'
 import { sleep, check } from 'k6'
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = {
     // Define the number of iterations for the test
@@ -19,10 +20,7 @@ export default function () {
   //login
     const url = 'http://localhost:3000/login'
 
-    const payload = JSON.stringify({    
-        username: "julio.lima",
-        senha: "123456",
-    })
+    const payload = JSON.stringify(postLogin)
 
     const params = {
         headers: {
